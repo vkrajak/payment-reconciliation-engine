d0ad7f6dc049bcdc9339a9b38f985f6a2b38eb1f
Vipin Rajak <vipinkumarrajak.vr@gmail.com>
2026-08-11 23:33:06 +0530

    Initial commit

---
+++ src/main/java/com/vipinrajak/payment_reconciliation_engine/PaymentReconciliationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.vipinrajak.payment_reconciliation_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentReconciliationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentReconciliationEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vipinrajak/payment_reconciliation_engine/PaymentReconciliationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipinrajak.payment_reconciliation_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentReconciliationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
